5bead22c51e1a69a4f2ca17570131d493247e42f
tbpotter <tpotte10@jhu.edu>
2015-02-22 12:50:32 -0500

    submitting final code for programming assignment #2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,60 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## this function creates a cached matrix object. the objects stores the
+## matrix and has functions that get/set the matrix values, get/set the matrix
+## inverse values and create a list of pairs.
 
 makeCacheMatrix <- function(x = matrix()) {
+    
+    m <- NULL
+    
+    ## sets a new value to this matrix. it erases any cached inverse values.
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    
+    ## the get() returns the matrix data
+    get <- function() x
+    
+    ## the setinverse() function "caches" the inverse of the matrix
+    setinverse <- function(inverse) m <<- inverse
+    
+    ## the getinverse() function returns the value of the matrix inverse
+    ## can be null
+    getinverse <- function() m
+    
+    
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 
 }
 
 
-## Write a short comment describing this function
+## This function will check the matrix object to see if its inverse
+## is already cached. If the inverse is cached it will return the result 
+## otherwise it will calculate it, cache it and return it.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    
+    ## get the inverse value from the matrix
+    m <- x$getinverse()
+    
+    ## if it exists (is not null) then return the cached value
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    
+    ## the inverse does not exist so we need to calculate it
+    ## get the matrix
+    data <- x$get()
+    
+    ## calculate the inverse of the matrx
+    m <- solve(data, ...)
+    
+    ## set the inverse value ("cache it")
+    x$setinverse(m)
+    
+    ## return the inverse value
+    m
 }
